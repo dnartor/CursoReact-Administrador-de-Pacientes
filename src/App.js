f80e0c5afb0ae6268038bc1131aa7f60b5f4cf9f
DanielNaranjo <dnartor.98@gmail.com>
2021-02-14 19:26:33 +0000

    Añadido local storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import Cita from "./components/Cita";
 
 function App() {
-  const [citas, guardarCitas] = useState([]);
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  if(!citasIniciales) {
+    citasIniciales = [];
+  }
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  useEffect(() => {
+    if(citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas));
+    }else{
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas]);
 
   const crearCita = (cita) => {
     guardarCitas([...citas, cita]);
   };
+
+  const eliminarCita = (id) => {
+    const nuevasCitas = citas.filter((cita) => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  };
+
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus citas";
   return (
     <Fragment>
       <h1>Administrador de Pacientes </h1>
@@ -18,13 +37,12 @@ function App() {
             <Formulario crearCita={crearCita} />
           </div>
           <div className="one-half column">
-            <h2>Administra tus citas</h2>
+            <h2>{titulo}</h2>
             {citas.map((cita) => (
-              <Cita key={cita.id}
-                  cita={cita}
-                  />
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
             ))}
           </div>
+          )
         </div>
       </div>
     </Fragment>
--- src/components/Cita.js
+++ src/components/Cita.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const Cita = ({cita}) => {
-    return(
+const Cita = ({ cita, eliminarCita}) => {
+  return (
     <div className="cita">
-        <p>Mascota: <span>{cita.mascota}</span></p>
-        <p>Dueño: <span>{cita.propietario}</span></p>
-        <p>Fecha: <span>{cita.fecha}</span></p>
-        <p>Hora: <span>{cita.hora}</span></p>
-        <p>Síntomas: <span>{cita.sintomas}</span></p>
-    
+      <p>
+        Mascota: <span>{cita.mascota}</span>
+      </p>
+      <p>
+        Dueño: <span>{cita.propietario}</span>
+      </p>
+      <p>
+        Fecha: <span>{cita.fecha}</span>
+      </p>
+      <p>
+        Hora: <span>{cita.hora}</span>
+      </p>
+      <p>
+        Síntomas: <span>{cita.sintomas}</span>
+      </p>
+
+      <button 
+      className="button eliminar u-full-width"
+      onClick={() => eliminarCita(cita.id)}
+      >Eliminar</button>
     </div>
-    )
-}
- 
+  );
+};
+
 export default Cita;
